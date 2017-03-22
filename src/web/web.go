5ec1643bb87daa5132fdeeeb1391871a02925493
Julien Senon <julien.senon@gmail.com>
2017-03-22 23:09:03 +0100

    cleaning

--- src/web/web.go
+++ src/web/web.go
@@ -7,60 +7,7 @@ import (
 	"db"
 	// "encoding/json"
 	"fmt"
-	"gopkg.in/mgo.v2/bson"
 )
-
-type Server struct {
-	ID               bson.ObjectId        `json:"id" bson:"_id,omitempty"`
-	CMDBName         string               `json:"CMDBName"`
-	Function         string               `json:"Function"`
-	SerialNumber     string               `json:"SerialNumber"`
-	AssetCode        int                  `json:"Assetcode"`
-	HardwareRows     []HardwareDefinition `json:"Hardwarerows"`
-	LocalisationRows []Localisation       `json:"Localisationrows"`
-	NetworksRows     []Networks           `json:"Networksrows"`
-	Remarks          string               `json:"Remarks"`
-	Status           string               `json:"Status"`
-}
-
-type HardwareDefinition struct {
-	Model string `json:"Model"`
-	CPU   string `json:"CPU"`
-	RAM   string `json:"RAM"`
-}
-
-type Localisation struct {
-	Room     string `json:"Room"`
-	Building string `json:"Building"`
-	Rack     string `json:"Rack"`
-}
-
-type Networks struct {
-	IpAddr     string `json:"Ipaddr"`
-	PatchPanel string `json:"Patchpanel"`
-	ServerPort string `json:"Serverport"`
-	Switch     string `json:"Switch"`
-	Vlan       int    `json:"Vlan"`
-	MAC        string `json:"MAC"`
-}
-
-type Network struct {
-	CMDBName     string `json:"NCMDBName"`
-	Function     string `json:"NFunction"`
-	SerialNumber string `json:"NSerialNumber"`
-	AssetCode    int    `json:"NAssetCode"`
-	Rows         []Localisation
-}
-
-func Render(w http.ResponseWriter, filename string, data interface{}) {
-	tmpl, err := template.ParseFiles(filename)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	if err := tmpl.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
 
 func Index(res http.ResponseWriter, req *http.Request) {
 	// var rs Server
